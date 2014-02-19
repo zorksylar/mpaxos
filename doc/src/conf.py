fee1c0d2dd0e43377257c46403bce59cbff8f144
msmummy <msmummy@gmail.com>
2014-02-19 01:21:44 -0500

    refine the theme options

--- doc/src/conf.py
+++ doc/src/conf.py
@@ -99,7 +99,13 @@ html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+        'navbar_site_name': 'Doc',
+        'navbar_sidebarrel': False,
+        'navbar_pagenav': False,
+        'globaltoc_depth': 2,
+        'source_link_position': "exclude",
+        }
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
