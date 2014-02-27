ba8f6a8d6c690ab9701b75c6820b61f5b057e1b8
zorksylar <pin.gao2008@gmail.com>
2014-02-27 15:00:33 +0800

    add apps

---
+++ apps/hello_mpaxos.c
@@ -0,0 +1,28 @@
+#include <mpaxos.h>
+
+char data[20] = "Hello MPaxos!\n";
+int exit = 0;
+
+void cb(mpaxos_req_t *req) {
+    printf("%s", req->data); 
+    exit = 1;
+}
+
+int main () {
+    mpaxos_init();
+    mpaxos_config_load("mpaxos.cfg");
+    mpaxos_config_set("nodename", "node1");
+    mpaxos_set_cb_god(cb);
+    mpaxos_start();
+    mpaxos_req_t req;
+    memset(&req, 0, sizeof(mpaxos_req_t);
+    req->data = data;
+    req->sz_data = 20;
+     
+    mpaxos_commit_req(req);
+    while (!exit)  {
+        sleep(1);
+    }
+    mpaxos_destroy(); 
+} 
+
