a372edf3eba86633874036076d61bbb42eba028b
zorksylar <pin.gao2008@gmail.com>
2014-03-17 11:48:33 +0800

    make hello_mpaxos worked

--- apps/hello_mpaxos.c
+++ apps/hello_mpaxos.c
@@ -1,28 +1,28 @@
 #include <mpaxos.h>
+#include <unistd.h>
 
-char data[20] = "Hello MPaxos!\n";
-int do_exit = 0;
+unsigned char data_[20] = "Hello MPaxos!\n";
+int exit_ = 0;
 
 void cb(mpaxos_req_t *req) {
-    printf("%s", req->data); 
-    do_exit = 1;
+    printf("%s", req->data);
+    exit_ = 1;
 }
 
 int main () {
     mpaxos_init();
-    mpaxos_config_load("./apps/mpaxos.cfg");
+    mpaxos_config_load("mpaxos.cfg");
     mpaxos_config_set("nodename", "node1");
     mpaxos_set_cb_god(cb);
     mpaxos_start();
-    mpaxos_req_t *req;
+    mpaxos_req_t req;
     memset(&req, 0, sizeof(mpaxos_req_t));
-    req->data = data;
-    req->sz_data = 20;
-     
-    mpaxos_commit_req(req);
-    while (!do_exit)  {
+
+    req.data = data_;
+    req.sz_data = 20;
+    mpaxos_commit_req(&req);
+    while (!exit_)  {
         sleep(1);
     }
-    mpaxos_destroy(); 
-} 
-
+    mpaxos_destroy();
+}
